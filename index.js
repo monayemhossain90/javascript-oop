400566c2a23e081d33d8f2b2fab60d79dcc63450
monayemhossain90 <monayemhossain1990@gmail.com>
2024-10-28 06:16:34 +0600

    js oop

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const person = {
+    firstname:"mm",
+    lastname:"hossain",
+    age:32,
+    getName: ()=>{
+        return `${person.firstname}  ${person.lastname}`
+    },
+
+}
+
+console.log(person.getName())
